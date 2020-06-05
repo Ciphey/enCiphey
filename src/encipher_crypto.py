75deda4745977a1be57c38fed09437ed6ef14a5e
Brandon <brandonskerritt51@gmail.com>
2020-06-05 13:01:52 +0100

    Added encryptions from NN

--- src/encipher_crypto.py
+++ src/encipher_crypto.py
@@ -1,3 +1,4 @@
+import base64
 class encipher_crypto():
 
     """Holds the encryption functions
@@ -11,6 +12,49 @@ class encipher_crypto():
 
     def __init__(self):
         """TODO: to be defined. """
-        .__init__(self)
 
-        
+    def toBase64(self, text: str) -> str:
+        """Turns text into base64 using Python libray
+
+            args:
+                text -> text to convert
+
+            returns:
+                text -> as base 64"""
+        return base64.b64encode(bytes(text, 'utf-8'))
+     def caesar_cipher(s, k):
+        """Iterates through each letter and constructs the cipher text"""
+        new_message = ""
+        factor = k % 26
+        for c in s:
+            new_message += apply_rotation(c, factor)
+        return new_message
+
+       def sha1hash(s):
+            temp = str.encode(s)
+            temp = hashlib.sha1(temp)
+            return temp.hexdigest()
+
+        def md5hash(s):
+            temp = str.encode(s)
+            temp = hashlib.md5(temp)
+            return temp.hexdigest()
+
+        def sha256hash(s):
+            temp = str.encode(s)
+            temp = hashlib.sha256(temp)
+            return temp.hexdigest()
+
+        def sha512hash(s):
+            temp = str.encode(s)
+            temp = hashlib.sha512(temp)
+            return temp.hexdigest()
+
+        def apply_rotation(c, factor):
+            """Applies a shift of factor to the letter denoted by c"""
+            if c.isalpha():
+                lower = ord("A") if c.isupper() else ord("a")
+                c = chr(lower + ((ord(c) - lower + factor) % 26))
+            return c
+
+
